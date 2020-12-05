f76fde5db44b1b6e5e9ad8ca4997cdaf051f7841
Tim <kraemerstim@gmail.com>
2020-12-05 12:49:46 +0100

    Added solution for day 5

---
+++ src/day5/Solution5.java
@@ -0,0 +1,35 @@
+package day5;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static utility.AOCUtility.readFile;
+
+public class Solution5 {
+    public static void main(String[] args) {
+        var fileName = "resources/input/day5.txt";
+        List<String> input = readFile(fileName);
+
+        List<Integer> result = input.stream().map(Solution5::binaryConverter).collect(Collectors.toList());
+        int solutionA = result.stream().mapToInt(v -> v).max().orElse(-1);
+
+        System.out.println("SolutionA = " + solutionA);
+
+        for (int i = 0; i < solutionA; i++) {
+            if (!result.contains(i) && result.contains(i-1) && result.contains(i+1)){
+                System.out.println("SolutionB = " + i);
+            }
+        }
+    }
+
+    private static int binaryConverter(String s) {
+        var binary1 = s.chars().mapToObj(i->(char)i).map((c) -> {
+            if (c.equals('B') || c.equals('R'))
+                return "1";
+            else
+                return "0";
+        }).collect(Collectors.joining());
+
+        return Integer.parseInt(binary1, 2);
+    }
+}
